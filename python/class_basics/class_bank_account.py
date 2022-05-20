4c4452d2d70c8f61303f4d9a20c8f52af7f54702
Ryan <rjamesbradshaw@gmail.com>
2022-05-20 11:48:01 -0700

    bank account class added

---
+++ python/class_basics/class_bank_account.py
@@ -0,0 +1,61 @@
+class BankAccount:
+    def __init__(self, account_name, balance, int_rate):
+        self.account_name = account_name
+        self.balance = balance
+        self.int_rate = int_rate
+    
+    @staticmethod
+    def can_withdraw(balance, amount):
+        if((balance - amount) > 0):
+            return True
+        else:
+            return False
+
+    def check_balance(self):
+        print("The current balance of this account is: " + str(self.balance))
+        return self
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("$" + str(amount) + " has been deposited to your account")
+        self.check_balance()
+        return self
+    
+    def withdraw(self, amount):
+        if BankAccount.can_withdraw(self.balance, amount):
+            self.balance -= amount
+            print("$" + str(amount) + " has been successfully withdrawn." )
+            self.check_balance()
+            return self
+
+    def transfer(self, amount, target):
+        if(BankAccount.can_withdraw(self.balance, amount)):
+            self.balance -= amount
+            target.deposit(amount)
+            print("$" + str(amount) + "has been transferred to" + target.account_name)
+            self.check_balance()
+            return self
+
+    def yield_interest(self):
+        if(self.balance > 0):
+            self.balance += (self.balance * self.int_rate)
+        else:
+            print("$0.00 balance cannot accrue interest")
+        return self
+    
+    def account_info(self):
+        print("Account Owner: " + self.account_name)
+        print("Interest Rate: " + str(self.int_rate * 100)+"%")
+        print("Current Balance: $" + str(self.balance))
+
+ryan_account = BankAccount("Ryan", 0, 0.10)
+brandi_account = BankAccount("Brandi", 0, 0.25)
+
+ryan_account.account_info()
+
+ryan_account.deposit(1000).deposit(2000).deposit(3000).withdraw(500).yield_interest().check_balance()
+
+
+
+
+
--- python/class_basics/class_user.py
+++ python/class_basics/class_user.py
@@ -5,30 +5,37 @@ class User:
     
     def current_balance(self):
         print(self.name +"'s current balance is : $" + str(self.account_balance))
+        return self
     
     def make_deposit(self, amount):
         self.account_balance += amount
         print("$" + str(self.account_balance) + " has been deposited")
         self.current_balance()
+        return self
 
     def make_withdrawal(self, amount):
         if(amount > self.account_balance):
             print("Insufficient Funds for that Withdrawal")
             self.current_balance()
+            return self
         else:
             self.account_balance -= amount
             self.current_balance()
+            return self
 
     def make_transfer(self, amount, recipient):
         if(amount > self.account_balance):
             print("Insufficient Funds for that Withdrawal")
             self.current_balance()
+            return self
         if(amount < 0):
             print("Transfers must be only in positive amounts")
+            return self
         else:
             self.make_withdrawal(amount)
             recipient.account_balance += amount
             print("$" + str(amount) + " has been transferred to " + recipient.name)
+            return self
 #-----------------------------------------
 ryan = User("Ryan")
 brad = User("Brad")
