c39c0f7bb7e049970af3a3f0c6764871c32eccfc
Ryan <rjamesbradshaw@gmail.com>
2022-05-19 21:25:18 -0700

    User class created w/ test cases

---
+++ python/users_bank_accounts/class_user.py
@@ -0,0 +1,56 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account_balance = 0
+    
+    def current_balance(self):
+        print(self.name +"'s current balance is : $" + str(self.account_balance))
+    
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        print("$" + str(self.account_balance) + " has been deposited")
+        self.current_balance()
+
+    def make_withdrawal(self, amount):
+        if(amount > self.account_balance):
+            print("Insufficient Funds for that Withdrawal")
+            self.current_balance()
+        else:
+            self.account_balance -= amount
+            self.current_balance()
+
+    def make_transfer(self, amount, recipient):
+        if(amount > self.account_balance):
+            print("Insufficient Funds for that Withdrawal")
+            self.current_balance()
+        if(amount < 0):
+            print("Transfers must be only in positive amounts")
+        else:
+            self.make_withdrawal(amount)
+            recipient.account_balance += amount
+            print("$" + str(amount) + "has been transferred to " + recipient.name)
+#-----------------------------------------
+ryan = User("Ryan")
+brad = User("Brad")
+spencer = User("Spencer")
+#-----------------------------------------
+ryan.make_deposit(1000)
+ryan.make_deposit(2000)
+ryan.make_deposit(10000)
+ryan.make_withdrawal(500)
+ryan.current_balance()
+
+
+brad.make_deposit(5000)
+brad.make_deposit(10000)
+brad.make_withdrawal(300)
+brad.current_balance()
+
+spencer.make_deposit(99999)
+spencer.make_withdrawal(11111)
+spencer.make_withdrawal(22222)
+spencer.make_withdrawal(33333)
+
+ryan.make_transfer(5, spencer)
+ryan.current_balance
+spencer.current_balance
