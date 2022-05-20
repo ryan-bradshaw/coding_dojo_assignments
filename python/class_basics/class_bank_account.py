9a6ac72c024b1d9c944454622eff92cf964f2dad
Ryan <rjamesbradshaw@gmail.com>
2022-05-20 11:55:37 -0700

    added classmethod for all account info

--- python/class_basics/class_bank_account.py
+++ python/class_basics/class_bank_account.py
@@ -1,8 +1,16 @@
 class BankAccount:
+    all_accounts = []
+    
     def __init__(self, account_name, balance, int_rate):
         self.account_name = account_name
         self.balance = balance
         self.int_rate = int_rate
+        BankAccount.all_accounts.append(self)
+
+    @classmethod
+    def display_all(cls, accounts):
+        for account in accounts:
+            account.account_info()
     
     @staticmethod
     def can_withdraw(balance, amount):
@@ -55,7 +63,4 @@ ryan_account.account_info()
 
 ryan_account.deposit(1000).deposit(2000).deposit(3000).withdraw(500).yield_interest().check_balance()
 
-
-
-
-
+BankAccount.display_all(BankAccount.all_accounts)
